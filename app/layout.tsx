f7f24c6ac0fa29f46af382370966540b8b8138ca
AleksandrVakarin <aleksandrvakarin7@gmail.com>
2025-06-30 13:55:32 +0500

    fix layout meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-title" content="NextPWA" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-startup-image" href="/splash-screen.png" />
